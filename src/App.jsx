aa5287de976c7d6c0df9d2141d557cc7cb325834
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-25 19:20:55 +0530

    initial file

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core'
+import Header from './components/Header'
+import Coin from './pages/coin'
+import Home from './pages/home'
+import './App.css'
+
+function App() {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: '#e3e3e3',
+      // color: 'white',
+      minHeight: '100vh',
+    },
+  }))
+
+  const classes = useStyles()
+  return (
+    <div className={classes.App}>
+      <Router>
+        <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/coin/:id" component={Coin} />
+      </Router>
+    </div>
+  )
+}
+
+export default App
