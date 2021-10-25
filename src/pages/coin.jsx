63fe017df357c915de350eff0c37b2fa0d8294d8
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-25 19:23:05 +0530

    pages added

---
+++ src/pages/coin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Coin = () => {
+  return (
+    <div>
+      chase coin here
+    </div>
+  )
+}
+
+export default Coin
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/banner/Banner'
+import Carousel from '../components/banner/Carousel'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Carousel />
+    </div>
+  )
+}
+
+export default Home
