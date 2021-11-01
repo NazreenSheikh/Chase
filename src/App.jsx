8ad54579a296445fd168671adae6d00183343314
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-11-01 16:28:42 +0530

    migrated vite to cra

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core'
 import Header from './components/Header'
@@ -8,25 +7,25 @@ import Alert from './components/Alert'
 import './App.css'
 
 function App() {
-  const useStyles = makeStyles(() => ({
-    App: {
-      backgroundColor: '#e3e3e3',
-      // color: 'white',
-      minHeight: '100vh',
-    },
-  }))
+    const useStyles = makeStyles(() => ({
+        App: {
+            backgroundColor: '#e3e3e3',
+            // color: 'white',
+            minHeight: '100vh',
+        },
+    }))
 
-  const classes = useStyles()
-  return (
-    <div className={classes.App}>
-      <Router>
-        <Header />
-        <Route path="/" exact component={Home} />
-        <Route path="/coins/:id" component={Coin} />
-      </Router>
-      <Alert />
-    </div>
-  )
+    const classes = useStyles()
+    return (
+        <div className={classes.App}>
+            <Router>
+                <Header />
+                <Route path="/" exact component={Home} />
+                <Route path="/coins/:id" component={Coin} />
+            </Router>
+            <Alert />
+        </div>
+    )
 }
 
 export default App
---
+++ src/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import CryptoContext from './context/CryptoContext'
+import { createTheme, ThemeProvider } from '@material-ui/core'
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#fb8c35',
+        },
+        secondary: {
+            main: '#e3e3e3',
+        },
+    },
+})
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CryptoContext>
+                <App />
+            </CryptoContext>
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
+
+reportWebVitals()
